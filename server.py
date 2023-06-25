77fc58d129f95e3575d32036a69ddd01793f0f2f
Yogarajalakshmi <yogarajalakshmis@gmail.com>
2023-06-25 10:50:22 +0530

    updating contact page - flask POST request | HTML forms | SMTP

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
 
 app = Flask(__name__)
 
+FROM_EMAIL = os.environ.get('FROM_EMAIL')
+TO_EMAIL = os.environ.get('TO_EMAIL')
+PASSWORD = os.environ.get('PASSWORD')
 
 blogs = requests.get(url='https://api.npoint.io/619dadf06938e6832149').json()
 
@@ -17,9 +22,25 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message').strip()
+        # print(name+"\n"+email+"\n"+phone+"\n"+message)
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=FROM_EMAIL, password=PASSWORD)
+            connection.sendmail(
+                from_addr=FROM_EMAIL,
+                to_addrs=TO_EMAIL,
+                msg=f"Subject: New User Created\n\n{name}\n{email}\n{phone}\n{message}")
+            connection.close()
+        return render_template("contact.html", msg=True)
+    elif request.method == "GET":
+        return render_template("contact.html")
 
 
 @app.route("/post/<int:blog_id>")
